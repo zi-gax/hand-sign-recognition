67fb43f67ef3faf0ce3e3217795d3cf63f0e8718
zigax <khodezigax@gmail.com>
2026-02-04 21:23:21 +0330

    Real-time hand sign recognition

--- src/config.py
+++ src/config.py
@@ -3,8 +3,14 @@
 CAMERA_INDEX = 0
 
 # Paths
-CSV_PATH = "../data/hand_sign_dataset.csv"
-MODEL_PATH = "../models/knn_model.joblib"
+from pathlib import Path
+
+# Get the directory where this script resides
+BASE_DIR = Path(__file__).resolve().parent.parent  # adjust if main.py is in /src
+
+# Define paths relative to BASE_DIR
+CSV_PATH = BASE_DIR / "data" / "hand_sign_dataset.csv"
+MODEL_PATH = BASE_DIR / "models" / "knn_model.joblib"
 
 # MediaPipe
 MAX_HANDS = 1
